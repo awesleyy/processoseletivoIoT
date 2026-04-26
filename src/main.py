53e283d3cb7a1b10224230467ca28b57c5ce74d2
André Wesley <andre.wesley290@gmail.com>
2026-04-26 16:01:28 -0300

    Fix: Finalizando execução após 5 ciclos para validação do CI

--- src/main.py
+++ src/main.py
@@ -2,24 +2,24 @@ import machine
 import time
 
 # Configuração do hardware
+# O pino 2 é o padrão para o LED na placa ESP32 do Wokwi
 led = machine.Pin(2, machine.Pin.OUT)
 
 print("--- Sistema Iniciado com Sucesso ---")
 
-def loop():
-    contagem = 0
-    while True:
-        contagem += 1
+def executar_teste():
+    for contagem in range(1, 6):
         led.value(1)
-        print("LED ACESO | Ciclo:", contagem)
-        time.sleep(1)
+        print(f"LED ACESO | Ciclo: {contagem}")
+        time.sleep(0.5)
         
         led.value(0)
-        print("LED APAGADO")
-        time.sleep(1)
+        print(f"LED APAGADO | Ciclo: {contagem}")
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     try:
-        loop()
+        executar_teste()
+        print("--- Monitoramento Concluído com Sucesso ---")
     except Exception as e:
         print("Erro no sistema:", e)

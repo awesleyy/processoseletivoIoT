9c19eb174a69ac66c43e0f47f23c90baaf25be11
André Wesley <andre.wesley290@gmail.com>
2026-04-26 16:08:09 -0300

    Fix: Finalizando execução para aprovação do CI

--- src/main.py
+++ src/main.py
@@ -4,16 +4,15 @@ import time
 # Configuração do LED no pino 2
 led = machine.Pin(2, machine.Pin.OUT)
 
-print("--- Sistema de Monitoramento Iniciado ---")
+print("--- SISTEMA INICIADO ---")
 
-# Rodamos exatamente 5 vezes para que o teste termine com sucesso
 for i in range(1, 6):
-    led.value(1)
-    print(f"LED ACESO | Ciclo: {i}")
+    led.value(1) # Liga o LED
+    print(f"CICLO: {i} | LED: LIGADO")
     time.sleep(0.5)
     
-    led.value(0)
-    print(f"LED APAGADO | Ciclo: {i}")
+    led.value(0) # Desliga o LED
+    print(f"CICLO: {i} | LED: DESLIGADO")
     time.sleep(0.5)
 
-print("--- Projeto validado com sucesso! ---")
+print("--- TESTE CONCLUIDO COM SUCESSO ---")

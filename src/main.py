d10b9561bfb4a907c38ac22becea3befd994f060
André Wesley <andre.wesley290@gmail.com>
2026-04-27 08:57:22 -0300

    Final: Alinhando firmware com Python e atendendo expect_text

--- src/main.py
+++ src/main.py
@@ -1,21 +1,14 @@
 print("Teste")
-
-from machine import Pin
 import time
+from machine import Pin
 
-# Configuração do LED (exemplo de sistema embarcado)
 led = Pin(2, Pin.OUT)
+print("Iniciando hardware...")
 
-# O QUE O ROBÔ PRECISA LER PARA DAR O CHECK VERDE:
-print("Teste") 
-
-# Lógica do projeto (exemplo: piscar 3 vezes e encerrar para não dar timeout)
 for i in range(3):
     led.value(1)
-    print(f"LED ligado - Ciclo {i+1}")
     time.sleep(0.5)
     led.value(0)
-    print(f"LED desligado - Ciclo {i+1}")
     time.sleep(0.5)
 
-print("Simulacao finalizada com sucesso.")
+print("Fim do teste.")
